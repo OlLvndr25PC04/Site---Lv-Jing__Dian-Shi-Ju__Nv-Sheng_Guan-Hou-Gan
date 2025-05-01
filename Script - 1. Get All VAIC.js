a790e904e2d03ee49d412130462cfc0298bbcbba
OlLvndr25PC04 <OliviaLavender2025PC04@proton.me>
2025-05-01 05:58:17 +0000

    Adding 101 - 150 of 201 VAIC

--- Script - 1. Get All VAIC.js
+++ Script - 1. Get All VAIC.js
@@ -27,7 +27,7 @@ let totalDuration = j.reduce((acc, j) => {
 
 let totalN = Math.ceil(j.length / 50)
 
-let N = 1, inc = 50
+let N = 2, inc = 50
 let start = N * inc, end = (N + 1) * inc
 
 
@@ -104,7 +104,7 @@ let J = j.slice(start, end).map((j, i) => {
                 // console.log(i + 1, fnObj)
                 allFnObjs.push(fnObj)
                 state.json && r()
-            }, state.json ? 0 : 300 * i);
+            }, state.json ? 0 : 2500 * i);
 
         })
 
